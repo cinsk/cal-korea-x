c6bf5bda2fc9115baf0d8fa5ee6a03c85b34658f
Seong-Kook Shin <cinsky@gmail.com>
2011-09-23 18:26:31 +0900

    added: more functions

--- lunar-ko.el
+++ lunar-ko.el
@@ -24,11 +24,42 @@
 
 ;;; Code:
 
+;;
+;; First of all, I don't have enough knowledge of lunar calendar
+;; system of either Chinese lunar calendar or Korean lunar calendar.
+;; If you find any mistake in my code/documentation, please let me
+;; know.
+;; 
+
+;;
+;; AFAIK, the Korean lunar calendar is almost the same as Chinese
+;; lunar calendar.  They are different because, the Chinese one is
+;; based on the astronomical calculation is carried out for the
+;; meridian 120 degrees east of Greenwich, where the Korean one is
+;; based on for the meridian 135 degrees east of Greenwich.
+;;
+;; For example, the Chinese lunar new year's day in 1997 was Feb. 7th
+;; 1997 where the Korean lunar new year's day in 1997 was Feb. 8th
+;; 1997.
+;;
+;; AFAIK, there is no absolute perfect formula to convert the
+;; solar(gregorian) date into (Korean?) lunar date.  The official
+;; Korean lunar calendar is issued by Korea Astronomy and Space
+;; Science Institute, and it seems that they provides only web-based
+;; converting tools for the lunar dates from (1 1 1391) to (11 18
+;; 2050) .
+;;
 
 ;; LDATESPEC := (MONTH DAY YEAR LEAP)
 ;;            | (MONTH DAY YEAR)
+;;
+;; MONTH := 1-12
+;; DAY   := 1-30
+;; YEAR  := 1391-2050
+;; LEAP  := t or nil
 
 (require 'lunar-ko-mdays)
+(require 'lunar-ko-cache)
 
 (defconst lunar-ko-celestial-stem
   [("갑" . "甲")
@@ -57,6 +88,18 @@
                                        ("해" . "亥")]
    "Korean terrestrial branch (identical to that of Chinese)")
 
+
+(defun gregorian-to-julian (date)
+  "Convert gregorian date in the form of (MONTH DAY YEAR) to
+Julian day number.  See also `calendar-astro-date-string'."
+  (calendar-astro-from-absolute
+   (calendar-absolute-from-gregorian date)))
+  
+(defun julian-to-gregorian (date)
+  "Convert Julian day number to the gregorian date in the form
+of (MONTH DAY YEAR).  See also `calendar-astro-goto-day-number'."
+  (calendar-gregorian-from-absolute 
+   (floor (calendar-astro-to-absolute date))))
 
 (defun lunar-ko-find-nearest (value type &optional from to)
   "Find the nearest and less-than-or-equal entry of VALUE from
@@ -118,22 +161,93 @@ is a Korean lunar day number if TYPE is :lunar."
        (nth 3 ldatespec)))
 
 
-(defun lunar-ko-month-days (year month &optional leap)
-  "Return the number of days in Korean lunar date YEAR MONTH
+(defun lunar-ko-month-index (ldatespec &optional cache)
+  "Return the month index of the vector in `korean-lunar-months' as
+in LDATESPEC.
 
-If the optional LEAP is non-nil, MONTH is considered as a leap month.
+The optional CACHE specifies the pre-acquired hash entry if any."
+  (let* ((entry (or cache 
+                    (gethash (lunar-ko-year ldatespec) korean-lunar-months)))
+         (lmon (car entry))
+         (vec (cdr entry))
+         (month (lunar-ko-month ldatespec)))
+    (assert (< month 13) t "out of range: %S")
+    (assert (> month 0) t "out of range: %S")
+    (cond ((null lmon) (1- month))
+          ((< month lmon) (1- month))
+          ((and (= month lmon) (not (lunar-ko-leap? ldatespec))) (1- month))
+          (t month))))
 
-If LEAP is non-nil and the actual YEAR has no leap month
-in MONTH, LEAP is ignored."
-  (let* ((val (gethash year korean-lunar-months))
-         (lmon (car val))
-         (days (cdr val)))
-    (if lmon
-        (cond ((< month lmon) (aref days (1- month)))
-              ((> month lmon) (aref days month))
-              ((= month lmon) (aref days (- month (if leap 0 1)))))
-      (aref days (1- month)))))
+(defun lunar-ko-month-days (ldatespec &optional cache)
+  "Return the number of days in Korean lunar date, LDATESPEC
+
+The optional CACHE specifies the pre-acquired hash entry if any."
+  (let ((entry (or cache
+                   (gethash (lunar-ko-year ldatespec) korean-lunar-months))))
+    (aref (cdr entry) (lunar-ko-month-index ldatespec entry))))
+
+(defun lunar-ko-impl-days (impl &optional cache)
+  "Return the number of days in the lunar month from internal
+representation, IMPL.
+
+The optional CACHE specifies the pre-acquired hash entry if any."
+  (let ((entry (or cache
+                   (gethash (lunar-ko-year impl) korean-lunar-months))))
+    (aref (cdr entry) (lunar-ko-month impl))))
+
     
+(defun lunar-ko-date-to-impl (ldatespec &optional cache)
+  "Convert LDATESPEC into internal lunar format.
+
+The internal form is like (MONTH-INDEX DAY YEAR), where MONTH-INDEX
+is the index value of the vector in `korean-lunar-months' (0-12)."
+  (list (lunar-ko-month-index ldatespec)
+        (lunar-ko-day ldatespec)
+        (lunar-ko-year ldatespec)))
+
+(defun lunar-ko-impl-to-date (impl &optional cache)
+  "Convert the internal lunar value into LDATESPEC.
+
+The internal form is like (MONTH-INDEX DAY YEAR), where MONTH-INDEX
+is the index value of the vector in `korean-lunar-months' (0-12)."
+  (let* ((month (lunar-ko-month impl))
+         (day (lunar-ko-day impl))
+         (year (lunar-ko-year impl))
+         (entry (or cache
+                    (gethash year korean-lunar-months)))
+         (leap-month (car entry)))
+    (cond ((null leap-month) (list (1+ month) day year))
+          ((< month leap-month) (list (1+ month) day year))
+          ((= month leap-month) (list (1+ month) day year t))
+          ((> month leap-month) (list month day year)))))
+
+(defun lunar-ko-advance (ldatespec days)
+  "Return the lunar date, advanced DAYS days from LDATESPEC."
+  (let ((date (lunar-ko-date-to-impl ldatespec)))
+    (while (> days 0)
+      (let* ((year (lunar-ko-year date))
+             (entry (gethash year korean-lunar-months))
+             (vec (cdr entry))
+             (midx (lunar-ko-month date))
+             (remain-days (- (aref vec midx) (lunar-ko-day date))))
+        (let ((mdays (lunar-ko-impl-days date))
+              (day (+ (lunar-ko-day date) days)))
+          (if (<= day mdays)
+              ;; If there's no carry to MONTH
+              (setq days 0
+                    date (list midx day year)) ; FIN
+            ;; If there's carry from DAY to MONTH
+            (setq days (- days (- mdays (lunar-ko-day date)))
+                  day 0
+                  midx (1+ midx))
+            (if (< midx (length vec))
+                ;; If there's no carry to YEAR
+                (setq date (list midx day year)) ; no need to update the year
+              ;; If there's carry from MONTH to YEAR
+              (setq date (list 0 day (1+ year))))))))
+    (lunar-ko-impl-to-date date)))
+
+
 (defun lunar-ko-valid? (ldatespec)
   "Return t if the lunar date LDATESPEC is actually existed."
   (let* ((val (gethash (lunar-ko-year ldatespec) korean-lunar-months))
@@ -160,13 +274,13 @@ in MONTH, LEAP is ignored."
       (1- mon))))
 
 
-(defun lunar-ko-kld (ldatespec)
+(defun lunar-ko-date-to-kld (ldatespec)
   (+ (* (lunar-ko-year ldatespec) 100000)
      (* (lunar-ko-month ldatespec) 1000)
      (if (lunar-ko-leap? ldatespec) 100 0)
      (lunar-ko-day ldatespec)))
 
-(defun lunar-ko-date (kld)
+(defun lunar-ko-kld-to-date (kld)
   (list (/ (mod kld 100000) 1000)
         (mod kld 100)
         (/ kld 100000)
@@ -174,13 +288,21 @@ in MONTH, LEAP is ignored."
             t)))
 
 
+(defun lunar-ko-lunar-date (solar-date)
+  "Convert the solar date into the Korean lunar date."
+  (let* ((ajd (gregorian-to-julian solar-date))
+         (pair (lunar-ko-find-nearest ajd :solar)))
+    (lunar-ko-advance (lunar-ko-kld-to-date (cdr pair))
+                      (round (- ajd (car pair))))))
+
+
 (defun lunar-ko-days-to (ldatespec)
   "Return number of days from the beginning of the lunar year to LDATESPEC.
 
 The date of LDATESPEC is not counted."
-  (let* ((val (gethash (lunar-ko-year ldatespec) korean-lunar-months))
-         (vec (cdr val))
-         (idx (lunar-ko-month-index ldatespec))
+  (let* ((entry (gethash (lunar-ko-year ldatespec) korean-lunar-months))
+         (vec (cdr entry))
+         (idx (lunar-ko-month-index ldatespec entry))
          (days 0))
     (1- (+ (dotimes (i idx days)
              (setq days (+ days (aref vec i))))
@@ -191,9 +313,9 @@ The date of LDATESPEC is not counted."
   "Return number of days from LDATESPEC to the end of the lunar year.
 
 The date of LDATESPEC is counted."
-  (let* ((val (gethash (lunar-ko-year ldatespec) korean-lunar-months))
-         (vec (cdr val))
-         (idx (lunar-ko-month-index ldatespec))
+  (let* ((entry (gethash (lunar-ko-year ldatespec) korean-lunar-months))
+         (vec (cdr entry))
+         (idx (lunar-ko-month-index ldatespec entry))
          (days 0))
     (+ (- (aref vec idx) (lunar-ko-day ldatespec)) 
        1
